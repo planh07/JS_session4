850add39b8985716aadc295d154b0197e7d17583
planh07 <100529415+planh07@users.noreply.github.com>
2022-03-19 17:55:45 +0700

    update homework JS buổi 4

---
+++ BC25_LanAnh_JS_buoi4/main.js
@@ -0,0 +1,134 @@
+/**
+ * -input: tạo 3 biến a b c lấy dữ liệu từ người dùng
+ * -xử lý: 
+ * tạo 3 biến
+ * lớn 1
+ * lớn 2
+ * lớn 3
+ * -output: số lớn 1, số lớn 2, số nhỏ
+ */
+document.getElementById("get").onclick = function () {
+    var a = document.getElementById("so1").value * 1;
+    var b = document.getElementById("so2").value * 1;
+    var c = document.getElementById("so3").value * 1;
+    var lon1;
+    var lon2;
+    var lon3;
+    if (a > b) {
+        if (b > c) {
+            lon1 = a;
+            lon2 = b;
+            lon3 = c;
+        } else {
+            if (a > c) {
+                lon1 = a;
+                lon2 = c;
+                lon3 = b;
+            } else {
+                lon1 = c;
+                lon2 = a;
+                lon3 = b;
+            }
+        }
+
+    } else if (a < b) {
+        if (a > c) {
+            lon1 = b;
+            lon2 = a;
+            lon3 = c;
+        } else {
+            if (c > b) {
+                lon1 = c;
+                lon2 = b;
+                lon3 = a;
+            } else {
+                lon1 = b;
+                lon2 = c;
+                lon3 = a;
+            }
+        }
+
+    }
+    var ketqua = "Thứ tự sắp xếp là: " + lon1 + ">" + lon2 + ">" + lon3
+    document.getElementById("kq1").innerHTML = ketqua;
+}
+
+// Bài 2
+document.getElementById("btn").onclick = function () {
+    var pick = document.getElementById("pick").value;
+    var thongbao;
+    switch (pick) {
+        case "M":
+            thongbao = " Chào Mẹ";
+            break;
+        case "B":
+            thongbao = " Chào Ba";
+            break;
+        case "A":
+            thongbao = " Chào Anh Trai";
+            break;
+        case "E":
+            thongbao = " Chào Em gái";
+            break;
+        default:
+            thongbao = "Không bé ơi ^^";
+    }
+    document.getElementById("tbao").innerHTML = thongbao;
+}
+
+// Bài 3 
+const result2 = document.getElementById("result2");
+document.getElementById("btnFind").onclick = () => {
+  let so1 = document.getElementById("so1").value;
+  let so2 = document.getElementById("so2").value;
+  let so3 = document.getElementById("so3").value;
+  let sochan = 0;
+  let sole = 0;
+  let taphop = [so1, so2, so3];
+
+  for (let i = 0; i < taphop.length; i++) {
+    if (taphop[i] % 2 == 0) {
+      sochan++;
+    } else {
+      sole++;
+    }
+  }
+  result2.innerHTML = `Co ${sochan} so chan &
+                        Co ${sole} so le`;
+  result2.classList.add("final");
+};
+
+// Bài 4
+/**
+ * input: nhập 3 cạnh của 1 tam giác a b c
+ * xử lý: 
+ * - nếu a = b = c => tam giác đều
+ * - Nếu a = b || a = c || c = b {
+ *      nếu a*a + b*b = c*c  || a*a + c*c = b*b || b*b + c*c  = a*a => tam giác vuông cân
+ *      Còn lại => tam giác cân
+ * }
+ * - Nếu  nếu a*a + b*b = c*c  || a*a + c*c = b*b || b*b + c*c  = a*a => tam giác vuông
+ * - còn lại tam giác bt
+ * output: xuất ra kết quả 
+ * 
+ */
+document.getElementById("xem").onclick = function(){
+    var c1 = document.getElementById("canh1").value*1;
+    var c2 = document.getElementById("canh2").value*1;
+    var c3 = document.getElementById("canh3").value*1;
+    var kq3;
+    if (c1 == c2 && c2 == c3){
+        kq3 = "Đây là tam giác đều";
+    } else if ( c1 == c2 || c2 == c3  || c1 == c3){
+        if (c1*c1 + c2*c2 == c3*c3 || c2*c2 + c3*c3 == c1*c1 || c1*c1 + c3*c3 == c2*c2){
+            kq3 = "Đây là tam giác vuông cân";
+        } else {
+            kq3 = "Đây là tam giác cân";
+        }
+    } else if(c1*c1 + c2*c2 == c3*c3 || c2*c2 + c3*c3 == c1*c1 || c1*c1 + c3*c3 == c2*c2){
+        kq3 = "Đây là tam giác vuông";
+    } else {
+        kq3 = "Đây là tam giác thường";
+    }
+    document.getElementById("kq3").innerHTML = kq3;
+}
